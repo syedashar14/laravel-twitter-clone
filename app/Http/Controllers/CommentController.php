d195fa386db08f2d0a3af6e49c912577e69694b2
Ashar Abbas <ashar.abbas@RT-LT-336.rolustech.net>
2024-03-30 16:41:28 +0500

    Added form request features for Idea, user profile & Comments

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,18 +2,18 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateCommentRequest;
 use App\Models\Comment;
 use App\Models\Idea;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    public function store (Idea $idea) {
-        $comment = new Comment();
-        $comment->idea_id = $idea->id;
-        $comment->content = request()->get('content', '');
-        $comment->user_id = auth()->user()->id;
-        $comment->save();
+    public function store (CreateCommentRequest $request, Idea $idea) {
+        $validated = $request->validated();
+        $validated['user_id'] = auth()->id();
+        $validated['idea_id'] = $idea->id;
+        Comment::create($validated);
         return redirect()->route('ideas.show', $idea->id)->with('success', 'Comment created successfully');
     }
 }
---
+++ app/Http/Requests/UpdateUserRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateUserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return $this->user()->can('update', $this->user);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            'image' => 'image',
+            'bio' => 'nullable|min:1|max:255',
+            'name' => 'required|min:2|max:255'
+        ];
+    }
+}
