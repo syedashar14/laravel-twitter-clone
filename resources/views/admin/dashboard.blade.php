cf3c1554bed2baa34e6b5b83ee191bfc206ccdc3
Ashar Abbas <ashar.abbas@RT-LT-336.rolustech.net>
2024-04-07 20:15:19 +0500

    Added data/information widgets on admin dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,13 +1,35 @@
 @extends('layout.layout')
-@section('title', 'Admin Panel')
-@include('layout.nav')
+@section('title', 'Admin Dashboard')
 @section('content')
-<div class="row">
-    <div class="col-3">
-        @include('admin.shared.left-bar')
+    <div class="row">
+        <div class="col-3">
+            @include('admin.shared.left-bar')
+        </div>
+        <div class="col-9">
+            <h1>Admin Dashboard</h1>
+            <div class="row">
+                <div class="col-sm-6 col-md-4">
+                    @include('shared.widget', [
+                        'title' => 'Total Users',
+                        'icon' => 'fas fa-users',
+                        'data' => $totalUsers,
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include('shared.widget', [
+                        'title' => 'Total Idea',
+                        'icon' => 'fas fa-lightbulb',
+                        'data' => $totalIdeas,
+                    ])
+                </div>
+                <div class="col-sm-6 col-md-4">
+                    @include('shared.widget', [
+                        'title' => 'Total Comments',
+                        'icon' => 'fas fa-comment',
+                        'data' => $totalComments,
+                    ])
+                </div>
+            </div>
+        </div>
     </div>
-    <div class="col-9">
-        <h1> Admin Dashboard </h1>
-    </div>
-</div>
 @endsection
